4db411973eef18482ad866bcc186f29bb1c6ba83
Moondaeng <wper1028@gmail.com>
2019-06-03 13:06:01 +0000

    메시지 코드 별로 요청을 할 수 있게 바꿔놓음, 종료 요청시에도 id pwd를 입력해야하긴 하는데 그래도 대충 입력하면 종료는 잘 된다

--- serverTest/dummyClient.c
+++ serverTest/dummyClient.c
@@ -42,39 +42,57 @@ int main(int argc,char *argv[])
 		fprintf(stderr,"connect erorr\n");
 		exit(1);
 	}
-	fputs("join: 101, login: 100 \n", stdout);
-	scanf("%d", &loginMsg.msg_code);
-	fputs("id: ",stdout);
-	scanf("%s",loginMsg.id);
-	fputs("passward: ",stdout);
-	scanf("%s",loginMsg.pwd);
-
-	write(sock,&loginMsg,sizeof(struct loginRequest));
-
-	printf("send msg \n");
 	
-	while(str_len==0)
-	str_len=read(sock,&auth,sizeof(struct loginAuth));
-	
-	printf("auth.sid = %d\n", auth.sid);
-	printf("str_len : %d\n",str_len);
+	while(1) {
+		fputs("join: 101, login: 100, quit: 500 \n", stdout);
+		scanf("%d", &loginMsg.msg_code);
+		fputs("id: ",stdout);
+		scanf("%s",loginMsg.id);
+		fputs("passward: ",stdout);
+		scanf("%s",loginMsg.pwd);
 
-	if(auth.sid==-1)
-	{
-		printf("login failed\n");
+		write(sock,&loginMsg,sizeof(struct loginRequest));
+
+		printf("send msg \n");
+		while(str_len==0)
+			str_len=read(sock,&auth,sizeof(struct loginAuth));
+		//str_len=read(sock,&auth.sid,sizeof(int));
+		printf("auth.sid = %d\n", auth.sid);
+		printf("str_len : %d\n",str_len);
+		printf("auth.msg_code : %d\n",auth.msg_code);
+		
+		switch(auth.msg_code) // 서버에서 온 메시지 코드에 따른 처리
+		{	
+			case 110: // 로그인 성공
+				if (auth.msg_code == 110 && auth.sid == -1)
+				{
+				printf("login failed\n");
+				break;
+				}
+				printf("login success\n");
+				printf("sid : %d\n", auth.sid);
+				printf("ID : %s\n",auth.profile.id);
+				printf("win/lose : %d/%d\n",auth.profile.win,auth.profile.lose);
+				printf("kill/death : %d/%d\n",auth.profile.kill,auth.profile.death);
+			break;
+			
+			case 111: // 회원가입 성공
+				printf("join successs\n");
+			break;
+			
+			case 112: // 대기실 진입 요청
+				printf("fuck");
+			break;
+			
+			case 113: // 회원가입 실패
+				printf("join failed\n");
+			break;
+			
+			case 510: // 종료 요청
+				close(sock);
+				return 0;
+		
+		}
+	//	memset(&auth,0,sizeof(struct loginAuth));	
 	}
-	else
-	{
-		printf("login success\n");
-		printf("sid : %d\n", auth.sid);
-		printf("ID : %s\n",auth.profile.id);
-		printf("win/lose : %d/%d\n",auth.profile.win,auth.profile.lose);
-		printf("kill/death : %d/%d\n",auth.profile.kill,auth.profile.death);
-	}
-	
-	
-	while(1);
-	close(sock);
-
-	return 0;
 }
