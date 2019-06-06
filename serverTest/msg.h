17ce60ba393c7d50f3c945d238ce215d825e7820
Moondaeng <wper1028@gmail.com>
2019-06-06 04:28:20 +0000

    대기실 구조체들 추가함

--- serverTest/msg.h
+++ serverTest/msg.h
@@ -11,10 +11,29 @@ struct profile
 	int death;
 };
 
+//user에 대한 구조체, inRoom 에서 접속시 전송
+struct user //[3]
+{
+	char id[16];
+	int slot;
+	int win;
+	int lose;
+	int kill;
+	int death;
+};
+
+//room에 대한 구조체, roomList 요청 시 전송
+struct room //[4]
+{
+	int rid;
+	int ucount;
+	char hname[16];
+};
+
 #ifndef LOGIN
 #define LOGIN
 
-//로그인 메시지 원형
+//로그인 메시지 원형 40byte
 struct loginMsg
 {
 	int msg_code;
@@ -40,16 +59,52 @@ struct loginAuth
 	struct profile profile;
 };
 
-// 방 
-struct room
-{
-	int rid;
-
-	int ucount;
-	char hname[16];
-};
-
 #endif
 
+#ifndef LOBBY
+#define LOBBY
+
+
+//대기실 메시지 원형 120byte
+struct lobbyMsg
+{
+	int msg_code;
+
+	char data[116];
+};
+
+//대기실 요청 , 방 생성, 접속, 리스트 요청
+struct lobbyRequest
+{
+	int msg_code;
+	int rid;
+	int sid;
+};
+
+//대기실 방 개수, write 두 번 쓰기는 아까워서 만듬
+struct lobbyAuth
+{
+    int msg_code;
+    int roomCount;
+};
+
+//대기실 생성, 리스트 승인
+struct lobbyListAuth//[4]
+{
+	int msg_code;
+	struct room room[4];
+
+};
+
+//대기실 접속 승인
+struct lobbyEnterAuth//[3]
+{
+	int msg_code;
+    struct user user[3];
+
+};
+
+
+#endif
 
 #endif
