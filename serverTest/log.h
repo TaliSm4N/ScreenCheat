ca1d201c789dfde50447556753d78eb1931ebad6
Moondaeng <wper1028@gmail.com>
2019-06-17 10:13:35 +0000

    ingame과 합침

--- serverTest/log.h
+++ serverTest/log.h
@@ -3,9 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "udpMsg.h"
+#include "myBoolean.h"
+#include "location.h"
 
 void error_handling(char *);
 void Log(char *);
 void LogNum(char *,int);
+void LogUDPMsg(udpMsg);
+void LogLoc(loc);
 
 #endif
